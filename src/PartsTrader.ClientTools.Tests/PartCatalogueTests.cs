6930bf0f1b4fa36cc91e5ca3aaf90bc5dc538dae
Imran Ali <ashifalinz@gmail.com>
2019-12-16 11:18:44 +1300

    Commiting completed test

---
+++ src/PartsTrader.ClientTools.Tests/PartCatalogueTests.cs
@@ -0,0 +1,157 @@
+﻿using Moq;
+using NUnit.Framework;
+using PartsTrader.ClientTools.Api;
+using PartsTrader.ClientTools.Api.Data;
+using PartsTrader.ClientTools.Integration;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PartsTrader.ClientTools.Tests
+{
+    /// <summary>
+    /// Tests for <see cref="PartCatalogue" />.
+    /// </summary>
+    [TestFixture]
+    public class PartCatalogueTests
+    {
+        private Mock<IPartsTraderPartsService> mockPartsService;
+
+        [SetUp]
+        public void Setup()
+        {
+            List<PartSummary> compatibleParts = new List<PartSummary>() { 
+                new PartSummary(),
+                new PartSummary()
+            };
+            mockPartsService = new Mock<IPartsTraderPartsService>();
+            mockPartsService.Setup(x => x.FindAllCompatibleParts(It.IsAny<string>()))
+                .Returns(compatibleParts);
+
+        }
+
+        #region Validate Parts Number
+
+        [Test]
+        public void ValidatePartsNumber_ValidPart_AlphabeticPartCode()
+        {      
+            PartCatalogue catalogue = new PartCatalogue(mockPartsService.Object);
+
+            bool isValid = catalogue.ValidatePartNumber("1234-abcd");
+
+            Assert.IsTrue(isValid);
+        }
+
+        [Test]
+        public void ValidatePartsNumber_ValidPart_AlphanumericPartCode()
+        {
+            PartCatalogue catalogue = new PartCatalogue(mockPartsService.Object);
+
+            bool isValid = catalogue.ValidatePartNumber("4321-a1b2c3d4");
+
+            Assert.IsTrue(isValid);
+
+        }
+
+        [Test]
+        public void ValidatePartsNumber_InvalidPart_InvalidPartId()
+        {
+            PartCatalogue catalogue = new PartCatalogue(mockPartsService.Object);
+
+            bool isValid = catalogue.ValidatePartNumber("a234-abcd");
+
+            Assert.IsFalse(isValid);
+        }
+
+        [Test]
+        public void ValidatePartsNumber_InvalidPart_InvalidPartCode()
+        {
+            PartCatalogue catalogue = new PartCatalogue(mockPartsService.Object);
+
+            bool isValid = catalogue.ValidatePartNumber("1234-abc");
+
+            Assert.IsFalse(isValid);
+        }
+
+        #endregion
+
+        #region CheckExclusionList
+
+        [Test]
+        public void CheckExclusionList_IsExcluded1()
+        {
+            PartCatalogue catalogue = new PartCatalogue(mockPartsService.Object);
+
+            bool IsExcluded = catalogue.CheckExclusionList("1111-Invoice");
+
+            Assert.IsTrue(IsExcluded);
+        }
+
+        [Test]
+        public void CheckExclusionList_IsExcluded2()
+        {
+            PartCatalogue catalogue = new PartCatalogue(mockPartsService.Object);
+
+            bool IsExcluded = catalogue.CheckExclusionList("9999-charge");
+
+            Assert.IsTrue(IsExcluded);
+        }
+
+        [Test]
+        public void CheckExclusionList_IsNOTExcluded1()
+        {
+            PartCatalogue catalogue = new PartCatalogue(mockPartsService.Object);
+
+            bool IsExcluded = catalogue.CheckExclusionList("1234-testCharge");
+
+            Assert.IsFalse(IsExcluded);
+        }
+
+        [Test]
+        public void CheckExclusionList_IsNOTExcluded2()
+        {
+            PartCatalogue catalogue = new PartCatalogue(mockPartsService.Object);
+
+            bool IsExcluded = catalogue.CheckExclusionList("789-asdfjaskjldf");
+
+            Assert.IsFalse(IsExcluded);
+        }
+
+
+        #endregion
+
+        #region GetCompatibleParts
+
+        [Test]
+        public void GetCompatibleParts_IsExcluded_ReturnEmpty()
+        {
+            PartCatalogue catalogue = new PartCatalogue(mockPartsService.Object);
+
+            IEnumerable<PartSummary> compatibleParts = catalogue.GetCompatibleParts("1111-Invoice");
+
+            Assert.AreEqual(0, compatibleParts.Count());
+        }
+
+        [Test]
+        public void GetCompatibleParts_IsNOTExcluded_ReturnResults()
+        {
+            PartCatalogue catalogue = new PartCatalogue(mockPartsService.Object);
+
+            IEnumerable<PartSummary> compatibleParts = catalogue.GetCompatibleParts("1234-teas");
+
+            Assert.AreEqual(2, compatibleParts.Count());
+        }
+
+        [Test]
+        public void GetCompatibleParts_InvalidPartNumber_ExceptionThrown()
+        {
+            PartCatalogue catalogue = new PartCatalogue(mockPartsService.Object);
+
+            string invalidPartNumber = "123-jskldfj";
+
+            Assert.Throws<InvalidPartException>(() => catalogue.GetCompatibleParts(invalidPartNumber));
+        }
+
+        #endregion
+
+    }
+}
---
+++ src/PartsTrader.ClientTools/PartCatalogue.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Web.Script.Serialization;
+using PartsTrader.ClientTools.Api;
+using PartsTrader.ClientTools.Api.Data;
+using PartsTrader.ClientTools.Integration;
+
+namespace PartsTrader.ClientTools
+{
+    public class PartCatalogue : IPartCatalogue
+    {
+        private IPartsTraderPartsService partsService;
+
+        public PartCatalogue(IPartsTraderPartsService partsTraderPartsService)
+        {
+            partsService = partsTraderPartsService;
+        }
+
+        public IEnumerable<PartSummary> GetCompatibleParts(string partNumber)
+        {
+            bool isValidPartNumber = ValidatePartNumber(partNumber);
+            if (!isValidPartNumber)
+            {
+                throw new InvalidPartException("Invalid part number : " + partNumber + " provided. Please check part number and try again");
+            }
+
+            bool isPartInExclusionList = CheckExclusionList(partNumber);
+            if (isPartInExclusionList)
+            {
+                return new List<PartSummary>();
+            }
+
+            return partsService.FindAllCompatibleParts(partNumber);
+        }
+
+        public bool ValidatePartNumber(string partNumber)
+        {
+            if(partNumber == null)
+            {
+                return false;
+            }
+
+            Regex validFormat = new Regex(@"^[0-9][0-9][0-9][0-9]-[A-Za-z0-9][A-Za-z0-9][A-Za-z0-9][A-Za-z0-9]+$");
+
+            return validFormat.IsMatch(partNumber);
+        }
+
+        public bool CheckExclusionList(string partNumber)
+        {
+            List<PartSummary> partsSummaryList;
+            string executingDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            using (StreamReader sr = new StreamReader(@executingDirectory+"/Exclusions.json"))
+            {
+                string json = sr.ReadToEnd();
+                partsSummaryList = new JavaScriptSerializer().Deserialize<List<PartSummary>>(json);
+            }            
+
+            return partsSummaryList.Any(p => p.PartNumber == partNumber);
+        }
+    }
+}
